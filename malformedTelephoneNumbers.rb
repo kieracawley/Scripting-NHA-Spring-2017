5149a3d9f0d844d6a4b9a97d2379bb839b8d20c3
Kiera Cawley <thebluecat544@gmail.com>
2017-03-04 17:10:11 -0500

    Ruby Conventions

--- malformedTelephoneNumbers.rb
+++ malformedTelephoneNumbers.rb
@@ -1,12 +1,12 @@
 require "CSV"
 
-formattedNumbers = []
+formatted_numbers = []
 
 File.open("import_telephone_numbers.csv") do |csv|
 	csv.each do |line|
-		x = line.gsub(/[^0-9]/, "")[-10..-1]
-		formattedNumbers.push("1-#{x[0..2]}-#{x[3..5]}-#{x[6..9]}")
+		new_number = line.gsub(/[^0-9]/, "")[-10..-1]
+		formatted_numbers.push("1-#{new_number[0..2]}-#{new_number[3..5]}-#{new_number[6..9]}")
 	end
 end
 
-puts formattedNumbers
+puts formatted_numbers
