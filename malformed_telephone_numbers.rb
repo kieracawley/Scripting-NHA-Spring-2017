f599c98075c83c37f4214c8d63912dd359196e0f
Kiera Cawley <thebluecat544@gmail.com>
2017-03-05 09:43:57 -0500

    Replaced Tabs With Spaces

--- malformed_telephone_numbers.rb
+++ malformed_telephone_numbers.rb
@@ -3,14 +3,14 @@ require "CSV"
 formatted_numbers = []
 
 File.open("import_telephone_numbers.csv") do |csv|
-	csv.each do |line|
-		new_number = line.gsub(/[^0-9]/, "")[-10..-1]
-		formatted_numbers.push("1-#{new_number[0..2]}-#{new_number[3..5]}-#{new_number[6..9]}")
-	end
+  csv.each do |line|
+    new_number = line.gsub(/[^0-9]/, "")[-10..-1]
+    formatted_numbers.push("1-#{new_number[0..2]}-#{new_number[3..5]}-#{new_number[6..9]}")
+  end
 end
 
 CSV.open("formatted_numbers.csv", "wb") do |csv|
-	formatted_numbers.each do |line|
-		csv << [line]
-	end
+  formatted_numbers.each do |line|
+    csv << [line]
+  end
 end
