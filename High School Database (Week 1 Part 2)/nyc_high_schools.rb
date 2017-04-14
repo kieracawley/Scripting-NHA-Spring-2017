d85bffbe83c6c846dd28e86fdd8c6b26eeb6b474
Kiera Cawley <thebluecat544@gmail.com>
2017-04-14 13:03:40 -0400

    Added Data To Map

--- High School Database (Week 1 Part 2)/nyc_high_schools.rb
+++ High School Database (Week 1 Part 2)/nyc_high_schools.rb
@@ -17,12 +17,10 @@ nyc_schools.each do |school|
 			"school_lowest_grade" => school[8],
 			"school_highest_grade" => school[9],
 			"school_address" => address,
+			"school_coodinate" => Geocoder.coordinates(address),
 			"district_name" => district[0],
-			"district_code" => school[0],
-			"district_percent_female" => district[3].to_f,
-			"district_percent_male" => 1 - district[3].to_f
+			"district_code" => school[0]
 		})
-		puts schools_formatted[-1]
 	end
 end
 
